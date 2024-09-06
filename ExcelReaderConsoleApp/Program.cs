35864b65a3387c819dad9c8ae7fb40c19687fb6a
Kyle <Kyle.Altemara@gmail.com>
2024-09-06 10:08:20 -0700

    Create solution

---
+++ ExcelReaderConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelReaderConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
