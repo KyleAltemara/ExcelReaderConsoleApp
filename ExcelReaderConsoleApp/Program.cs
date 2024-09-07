85731087eddf6ad55abed9e962dffbe45b31d016
Kyle <Kyle.Altemara@gmail.com>
2024-09-07 14:43:54 -0700

    Code cleanup

--- ExcelReaderConsoleApp/Program.cs
+++ ExcelReaderConsoleApp/Program.cs
@@ -7,7 +7,6 @@ internal class Program
     static void Main(string[] args)
     {
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-        // Load the Excel package
         var directoryPath = "../../../../ExcelFiles";
         string[] excelFiles = Directory.GetFiles(directoryPath, "*.xlsx");
         foreach (var filePath in excelFiles)
@@ -15,10 +14,8 @@ internal class Program
             FileInfo fileInfo = new(filePath);
 
             using ExcelPackage package = new(fileInfo);
-            // Get the workbook
             var workbook = package.Workbook;
 
-            // Iterate through all named ranges
             foreach (var worksheet in workbook.Worksheets)
             {
                 foreach (var table in worksheet.Tables)
@@ -37,12 +34,10 @@ internal class Program
                     Console.WriteLine();
                     List<List<object>> tableData = [];
 
-                    // Iterate through each row in the table
                     for (int row = range.Start.Row + 1; row <= range.End.Row; row++)
                     {
                         List<object> rowData = [];
 
-                        // Iterate through each column in the row
                         for (int column = range.Start.Column; column <= range.End.Column; column++)
                         {
                             var cellValue = worksheet.Cells[row, column].Value;
@@ -51,8 +46,6 @@ internal class Program
 
                         tableData.Add(rowData);
                     }
-
-                    // Use the tableData list for further processing
                 }
             }
         }
