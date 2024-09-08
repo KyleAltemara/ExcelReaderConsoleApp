76002c40313f84182b29c6e5a8646ee098f4ca39
Kyle <Kyle.Altemara@gmail.com>
2024-09-08 11:56:45 -0700

    Program now reads in all excel workbooks in the excel files directory and reads in all tables from each workbook

--- ExcelReaderConsoleApp/Program.cs
+++ ExcelReaderConsoleApp/Program.cs
@@ -11,8 +11,12 @@ internal class Program
         string[] excelFiles = Directory.GetFiles(directoryPath, "*.xlsx");
         foreach (var filePath in excelFiles)
         {
-            FileInfo fileInfo = new(filePath);
+            if (Path.GetFileName(filePath).StartsWith("~$"))
+            {
+                continue;
+            }
 
+            FileInfo fileInfo = new(filePath);
             using ExcelPackage package = new(fileInfo);
             var workbook = package.Workbook;
 
@@ -24,30 +28,122 @@ internal class Program
                     Console.WriteLine($"Table Name: {table.Name}");
                     var range = table.Range;
                     List<string> tableHeaders = [];
-                    Console.Write("Table Headers: ");
                     for (int column = range.Start.Column; column <= range.End.Column; column++)
                     {
                         tableHeaders.Add(worksheet.Cells[range.Start.Row, column].Text);
-                        Console.Write($"{worksheet.Cells[range.Start.Row, column].Text} ");
                     }
 
-                    Console.WriteLine();
-                    List<List<object>> tableData = [];
-
-                    for (int row = range.Start.Row + 1; row <= range.End.Row; row++)
+                    List<List<string>> tableData = [];
+                    for (int col = range.Start.Column; col <= range.End.Column; col++)
                     {
-                        List<object> rowData = [];
+                        List<string> columnData = [];
 
-                        for (int column = range.Start.Column; column <= range.End.Column; column++)
+                        for (int row = range.Start.Row + 1; row <= range.End.Row; row++)
                         {
-                            var cellValue = worksheet.Cells[row, column].Value;
-                            rowData.Add(cellValue);
+                            var cellValue = worksheet.Cells[row, col].Text;
+                            columnData.Add(cellValue);
                         }
 
-                        tableData.Add(rowData);
+                        tableData.Add(columnData);
                     }
+
+                    List<(Type, List<object>)> columnDataWithType = [];
+                    for (int col = 0; col < tableHeaders.Count; col++)
+                    {
+                        var columnType = GetColumnType(tableData[col]);
+                        List<object> data = [];
+                        columnDataWithType.Add((columnType, data));
+                        for (int row = 0; row < tableData[col].Count; row++)
+                        {
+                            var cellValue = tableData[col][row];
+                            if (columnType == typeof(int))
+                            {
+                                if (string.IsNullOrWhiteSpace(cellValue))
+                                {
+                                    data.Add(0);
+                                }
+                                else
+                                {
+                                    data.Add(int.Parse(cellValue));
+                                }
+                            }
+                            else if (columnType == typeof(double))
+                            {
+                                if (string.IsNullOrWhiteSpace(cellValue))
+                                {
+                                    data.Add(0.0);
+                                }
+                                else
+                                {
+                                    data.Add(double.Parse(cellValue));
+                                }
+                            }
+                            else if (columnType == typeof(DateTime))
+                            {
+                                if (string.IsNullOrWhiteSpace(cellValue))
+                                {
+                                    data.Add(DateTime.MinValue);
+                                }
+                                else
+                                {
+                                    data.Add(DateTime.Parse(cellValue));
+                                }
+                            }
+                            else
+                            {
+                                data.Add(cellValue);
+                            }
+                        }
+                    }
+
+                    var tableName = $"{worksheet.Name}_{table.Name}";
+
                 }
             }
         }
+    }
+
+    static Type GetColumnType(List<string> columnData)
+    {
+        Type? columnType = null;
+
+        foreach (var value in columnData)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                continue;
+            }
+
+            if (int.TryParse(value, out _))
+            {
+                if (columnType == typeof(double) || columnType != typeof(DateTime))
+                {
+                    continue;
+                }
+
+                columnType = typeof(int);
+            }
+            else if (double.TryParse(value, out _))
+            {
+                if (columnType == typeof(DateTime))
+                {
+                    continue;
+                }
+
+                columnType = typeof(double);
+            }
+            else if (DateTime.TryParse(value, out _))
+            {
+                columnType = typeof(DateTime);
+            }
+            else
+            {
+                columnType = typeof(string);
+                break;
+            }
+
+        }
+
+        return columnType is null ? typeof(string) : columnType;
     }
 }
